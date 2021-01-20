313e111c32b52c8d1c1e7d1d23aee3cdf91cd3f4
AristoK3 <mas.jordan@yandex.com>
2021-01-20 11:01:33 +0900

    remove tour mechanism

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -20,59 +20,6 @@
                 });
             });
         });
-        $(document).ready(function () {
-            if(!getCookie("step_dashboard")) {
-                $("#clients").addClass("in");
-                // Instance the tour
-                var tour = new Tour({
-                    storage: false,
-                    backdrop: true,
-                    steps: [
-                        {
-                            element: ".col-lg-12",
-                            title: "{{trans("Dashboard")}}",
-                            content: "{{trans("This is your dashboard, which you can use to get a fast and nice overview, of all your tasks, leads, etc.")}}",
-                            placement: 'top'
-                        },
-                        {
-                            element: "#myNavmenu",
-                            title: "{{trans("Navigation")}}",
-                            content: "{{trans("This is your primary navigation bar, which you can use to get around Daybyday CRM")}}"
-                        }
-                    ]
-                });
-
-                var canCreateClient = '{{ auth()->user()->can('client-create') }}';
-                if(canCreateClient) {
-                    tour.addSteps([
-                        {
-                            element: "#newClient",
-                            title: "{{trans("Create New Client")}}",
-                            content: "{{trans("Let's take our first step, by creating a new client")}}"
-                        },
-                        {
-                            path: '/clients/create'
-                        }
-                    ])
-                }
-
-                // Initialize the tour
-                tour.init();
-
-                tour.start();
-                setCookie("step_dashboard", true, 1000)
-            }
-            function setCookie(key, value, expiry) {
-                var expires = new Date();
-                expires.setTime(expires.getTime() + (expiry * 24 * 60 * 60 * 2000));
-                document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
-            }
-
-            function getCookie(key) {
-                var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
-                return keyValue ? keyValue[2] : null;
-            }
-        });
     </script>
 @endpush
         <!-- Small boxes (Stat box) -->
