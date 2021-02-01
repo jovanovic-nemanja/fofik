1821496cdf2d9f308a36a5ec6f470fb318213f7a
AristoK3 <thundervolt.fanddy001@gmail.com>
2021-02-01 18:30:17 +0900

    opencv library import

--- app/Services/OpenCVService.php
+++ app/Services/OpenCVService.php
@@ -7,6 +7,10 @@ use DB;
 
 use App\Models\CVPhoto;
 
+use CV\Face\LBPHFaceRecognizer, CV\CascadeClassifier, CV\Scalar, CV\Point;
+use function CV\{imread, cvtColor, equalizeHist};
+use const CV\{COLOR_BGR2GRAY};
+
 class OpenCVService extends BaseService
 {
     public function __construct()
