5f8aa8de895008cccc54d0041e16ef626dacda0e
AristoK3 <thundervolt.fanddy001@gmail.com>
2021-02-03 04:02:34 +0900

    cv_photos update

--- app/Services/OpenCVService.php
+++ app/Services/OpenCVService.php
@@ -8,7 +8,7 @@ use DB;
 use App\Models\CVPhoto;
 
 use CV\Face\LBPHFaceRecognizer, CV\CascadeClassifier, CV\Scalar, CV\Point;
-use function CV\{imread, cvtColor, equalizeHist};
+use function CV\{imread, imwrite, cvtColor, equalizeHist};
 use const CV\{COLOR_BGR2GRAY};
 
 class OpenCVService extends BaseService
@@ -24,61 +24,89 @@ class OpenCVService extends BaseService
 
         $faceRecognizer = LBPHFaceRecognizer::create();
 
-        $labels = ['unknown', 'me', 'angelina'];
-
-        // me
-        $src = imread(public_path('uploads/cv_photos/faces.jpg'));
-        $gray = cvtColor($src, COLOR_BGR2GRAY);
-        $faceClassifier->detectMultiScale($gray, $faces);
-        //var_export($faces);
-        equalizeHist($gray, $gray);
+        $gates = ['g1.jpg', 'g2.jpg', 'g3.jpg', 'g4.jpg', 'g5.jpg', 'g6.jpg', 'g7.jpg', 'g8.jpg', 'g9.jpg'];
+        $test = ['t1.jpg', 't2.jpg', 't3.jpg', 't4.jpg', 't5.jpg'];
+        $labels = ['unknown', 'Bill Gates'];
         $faceImages = $faceLabels = [];
-        foreach ($faces as $k => $face) {
-            $faceImages[] = $gray->getImageROI($face); // face coordinates to image
-            $faceLabels[] = 1; // me
-            //cv\imwrite("results/recognize_face_by_lbph_me$k.jpg", $gray->getImageROI($face));
+
+        foreach ($gates as $photo) {
+            $src = imread(public_path('uploads/cv_photos/'.$photo));    
+            $gray = cvtColor($src, COLOR_BGR2GRAY);
+            $faceClassifier->detectMultiScale($gray, $faces);    
+            equalizeHist($gray, $gray);
+            foreach ($faces as $k => $face) {
+                $faceImages[] = $gray->getImageROI($face); // face coordinates to image
+                $faceLabels[] = 1;
+            }
         }
         $faceRecognizer->train($faceImages, $faceLabels);
-
-        // angelina
-        $src = imread(publich_path('uploads/cv_photos/angelina_faces.png'));
-        $gray = cvtColor($src, COLOR_BGR2GRAY);
-        $faceClassifier->detectMultiScale($gray, $faces);
-        //var_export($faces);
-        equalizeHist($gray, $gray);
-        $faceImages = $faceLabels = [];
-        foreach ($faces as $k => $face) {
-            $faceImages[] = $gray->getImageROI($face); // face coordinates to image
-            $faceLabels[] = 2; // Angelina
-            //cv\imwrite("results/recognize_face_by_lbph_angelina$k.jpg", $gray->getImageROI($face));
+ 
+        foreach ($test as $photo) {
+            $src = imread(public_path('uploads/cv_photos/'.$photo));    
+            $gray = cvtColor($src, COLOR_BGR2GRAY);
+            $faceClassifier->detectMultiScale($gray, $faces);   
+            equalizeHist($gray, $gray);
+            foreach ($faces as $k => $face) {
+                $faceImage = $gray->getImageROI($face); // face coordinates to image
+                $faceLabel = $faceRecognizer->predict($faceImage, $faceConfidence);
+                echo $labels[$faceLabel]."\n";
+            }
         }
-        $faceRecognizer->update($faceImages, $faceLabels);
+        // $labels = ['unknown', 'me', 'angelina'];
 
-        //$faceRecognizer->write('results/lbph_model.xml');
-        //$faceRecognizer->read('results/lbph_model.xml');
+        // // me
+        // $src = imread(public_path('uploads/cv_photos/faces.jpg'));
+        // $gray = cvtColor($src, COLOR_BGR2GRAY);
+        // $faceClassifier->detectMultiScale($gray, $faces);
+        // //var_export($faces);
+        // equalizeHist($gray, $gray);
+        // $faceImages = $faceLabels = [];
+        // foreach ($faces as $k => $face) {
+        //     $faceImages[] = $gray->getImageROI($face); // face coordinates to image
+        //     $faceLabels[] = 1; // me
+        //     //cv\imwrite("results/recognize_face_by_lbph_me$k.jpg", $gray->getImageROI($face));
+        // }
+        // $faceRecognizer->train($faceImages, $faceLabels);
 
-        // test image
-        $src = imread(publich_path('uploads/cv_photos/angelina_and_me.png'));
-        $gray = cvtColor($src, COLOR_BGR2GRAY);
-        $faceClassifier->detectMultiScale($gray, $faces);
-        //var_export($faces);
-        equalizeHist($gray, $gray);
-        foreach ($faces as $face) {
-            $faceImage = $gray->getImageROI($face);
+        // // angelina
+        // $src = imread(public_path('uploads/cv_photos/angelina_faces.png'));
+        // $gray = cvtColor($src, COLOR_BGR2GRAY);
+        // $faceClassifier->detectMultiScale($gray, $faces);
+        // //var_export($faces);
+        // equalizeHist($gray, $gray);
+        // $faceImages = $faceLabels = [];
+        // foreach ($faces as $k => $face) {
+        //     $faceImages[] = $gray->getImageROI($face); // face coordinates to image
+        //     $faceLabels[] = 2; // Angelina
+        //     //cv\imwrite("results/recognize_face_by_lbph_angelina$k.jpg", $gray->getImageROI($face));
+        // }
+        // $faceRecognizer->update($faceImages, $faceLabels);
 
-            //predict
-            $faceLabel = $faceRecognizer->predict($faceImage, $faceConfidence);
-            echo "{$faceLabel}, {$faceConfidence}\n";
+        // //$faceRecognizer->write('results/lbph_model.xml');
+        // //$faceRecognizer->read('results/lbph_model.xml');
 
-            $scalar = new \CV\Scalar(0, 0, 255);
-            \CV\rectangleByRect($src, $face, $scalar, 2);
+        // // test image
+        // $src = imread(public_path('uploads/cv_photos/angelina_and_me.png'));
+        // $gray = cvtColor($src, COLOR_BGR2GRAY);
+        // $faceClassifier->detectMultiScale($gray, $faces);
+        // //var_export($faces);
+        // equalizeHist($gray, $gray);
+        // foreach ($faces as $face) {
+        //     $faceImage = $gray->getImageROI($face);
 
-            $text = $labels[$faceLabel];
-            \CV\rectangle($src, $face->x, $face->y, $face->x + ($faceLabel == 1 ? 50 : 130), $face->y - 30, new Scalar(255,255,255), -2);
-            \CV\putText($src, "$text", new Point($face->x, $face->y - 2), 0, 1.5, new Scalar(), 2);
-        }
+        //     //predict
+        //     $faceLabel = $faceRecognizer->predict($faceImage, $faceConfidence);
+        //     echo "{$faceLabel}, {$faceConfidence}\n";
 
-        cv\imwrite(public_path('uploads/cv_photos/results/_recognize_face_by_lbph.jpg'), $src);
+        //     $scalar = new \CV\Scalar(0, 0, 255);
+        //     \CV\rectangleByRect($src, $face, $scalar, 2);
+
+        //     $text = $labels[$faceLabel];
+        //     \CV\rectangle($src, $face->x, $face->y, $face->x + ($faceLabel == 1 ? 50 : 130), $face->y - 30, new Scalar(255,255,255), -2);
+        //     \CV\putText($src, "$text", new Point($face->x, $face->y - 2), 0, 1.5, new Scalar(), 2);
+        // }
+
+        // imwrite(public_path('uploads/cv_photos/results/_recognize_face_by_lbph.jpg'), $src);
         return null;
     }
 }
